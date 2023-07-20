7914347fa4b298d524e0ab9459dd1235596c6dc1
riyagupta2011 <riyagupta75751@gmail.com>
2023-07-20 16:38:16 +0530

    My initial commit

---
+++ sanity_backend/schemas/index.js
@@ -0,0 +1,9 @@
+import about from "./about";
+import brands from "./brands";
+import experiences from "./experiences";
+import skills from "./skills";
+import testimonials from "./testimonials";
+import work from "./work";
+import workExperience from "./workExperience";
+
+export const schemaTypes = [testimonials,experiences,about,brands,skills,work,workExperience]
